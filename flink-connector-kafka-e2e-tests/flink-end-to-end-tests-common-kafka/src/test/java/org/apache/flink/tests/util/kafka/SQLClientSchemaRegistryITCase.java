9c7593443549d3fbc78063703a7fe3d7b6b38ec9
MartijnVisser <martijnvisser@apache.org>
2022-10-18 15:35:04 +0200

    [FLINK-28405][Connector/Kafka] Update Confluent Platform images used for testing to v7.2.2. This closes #20170
    * [FLINK-28405][Connector/Kafka] Update Confluent Platform images used for testing to v7.2.2
    
    * [FLINK-28405][Connector/Kafka] Adding Guava to Schema Registry test since the Schema Registry client needs this, but doesn't bundle it
    
    * [FLINK-28405][Connector/Kafka] Refactored SchemaRegistryContainer so that we can include the Schema Registry container in DockerImageVersions
    
    * [FLINK-28405][Connector/Kafka] Make sure container gets cached

--- flink-connector-kafka-e2e-tests/flink-end-to-end-tests-common-kafka/src/test/java/org/apache/flink/tests/util/kafka/SQLClientSchemaRegistryITCase.java
+++ flink-connector-kafka-e2e-tests/flink-end-to-end-tests-common-kafka/src/test/java/org/apache/flink/tests/util/kafka/SQLClientSchemaRegistryITCase.java
@@ -70,6 +70,8 @@ public class SQLClientSchemaRegistryITCase {
             ResourceTestUtils.getResource(".*avro-confluent.jar");
     private final Path sqlConnectorKafkaJar = ResourceTestUtils.getResource(".*kafka.jar");
 
+    private final Path guavaJar = ResourceTestUtils.getResource(".*guava.jar");
+
     @ClassRule public static final Network NETWORK = Network.newNetwork();
 
     @ClassRule public static final Timeout TIMEOUT = new Timeout(10, TimeUnit.MINUTES);
@@ -83,7 +85,7 @@ public class SQLClientSchemaRegistryITCase {
 
     @ClassRule
     public static final SchemaRegistryContainer REGISTRY =
-            new SchemaRegistryContainer("6.2.2")
+            new SchemaRegistryContainer(DockerImageName.parse(DockerImageVersions.SCHEMA_REGISTRY))
                     .withKafka(INTER_CONTAINER_KAFKA_ALIAS + ":9092")
                     .withNetwork(NETWORK)
                     .withNetworkAliases(INTER_CONTAINER_REGISTRY_ALIAS)
@@ -252,7 +254,12 @@ public class SQLClientSchemaRegistryITCase {
     private void executeSqlStatements(List<String> sqlLines) throws Exception {
         flink.submitSQLJob(
                 new SQLJobSubmission.SQLJobSubmissionBuilder(sqlLines)
-                        .addJars(sqlAvroJar, sqlAvroRegistryJar, sqlConnectorKafkaJar)
+                        .addJars(
+                                sqlAvroJar,
+                                sqlAvroRegistryJar,
+                                sqlConnectorKafkaJar,
+                                sqlToolBoxJar,
+                                guavaJar)
                         .build());
     }
 }
