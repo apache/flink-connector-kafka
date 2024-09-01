dec48120a4930055a75738007627cc777f6d28eb
ClownXC <chen3494269@163.com>
2024-09-01 20:56:56 +0800

    optimize TopicPatternSubscriber

--- flink-connector-kafka/src/main/java/org/apache/flink/connector/kafka/source/enumerator/subscriber/KafkaSubscriberUtils.java
+++ flink-connector-kafka/src/main/java/org/apache/flink/connector/kafka/source/enumerator/subscriber/KafkaSubscriberUtils.java
@@ -23,6 +23,8 @@ import org.apache.kafka.clients.admin.TopicDescription;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /** The base implementations of {@link KafkaSubscriber}. */
 class KafkaSubscriberUtils {
@@ -35,6 +37,19 @@ class KafkaSubscriberUtils {
             return getTopicMetadata(adminClient, allTopicNames);
         } catch (Exception e) {
             throw new RuntimeException("Failed to get metadata for all topics.", e);
+        }
+    }
+
+    static Map<String, TopicDescription> getTopicMetadata(AdminClient adminClient, Pattern topicPattern) {
+        try {
+            Set<String> allTopicNames = adminClient.listTopics().names().get();
+            Set<String> patternTopicNames = allTopicNames.stream()
+                    .filter(topicName -> topicPattern.matcher(topicName).matches())
+                    .collect(Collectors.toSet());
+            return getTopicMetadata(adminClient, patternTopicNames);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    String.format("Failed to get metadata for %s topics.", topicPattern.pattern()), e);
         }
     }
 
